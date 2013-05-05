02b9757c5e76963d5d4551dd07b80cad7ac83c99
Joshua Lee <LeeJoshuaK@gmail.com>
2013-05-05 17:00:13 -0400

    dt player

--- player1/player.py
+++ player1/player.py
@@ -3,9 +3,11 @@ import data_collector
 import qlearning
 import simple_neural_net_player
 import em
+import dt
 
 def get_move(view):
-  return em.em_player.get_move(view)
+  return dt.simple_dt_player.get_move(view)
+  # return em.em_player.get_move(view)
   # return simple_neural_net_player.get_move(view)
   # return qlearning.get_move(view)
   # return data_collector.collect_data(view)
