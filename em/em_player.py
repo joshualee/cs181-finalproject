fe79ba968128c945994ab6d05f3116984f43aa3f
Joshua Lee <LeeJoshuaK@gmail.com>
2013-05-05 16:06:16 -0400

    create em player

---
+++ em/em_player.py
@@ -0,0 +1,38 @@
+import game_interface as gi
+import random
+import time
+import em
+
+DIRECTIONS = [
+  gi.UP,
+  gi.DOWN,
+  gi.LEFT,
+  gi.RIGHT
+]
+
+
+def set_prev_state(view):
+  view.prev_x = view.GetXPos()
+  view.prev_y = view.GetYPos()
+  
+  view.prev_life = view.GetLife()
+
+def get_move(view):
+  if view.GetRound() == 0:
+    view.em = em.EM()
+  else:
+    reward = view.GetLife() - view.prev_life
+    if reward > 0:
+      view.em.add_data_point(view.prev_x, view.prev_y)
+    
+    view.em.train(5)
+  
+  eat = False
+  if hasPlant and random.uniform(0, 1) < 0.5:
+    eat = True
+    
+  view.direction = view.em.get_direction(view)
+
+  set_prev_state(view)
+
+  return (view.direction, hasPlant)
--- player1/player.py
+++ player1/player.py
@@ -2,8 +2,10 @@ import common
 import data_collector
 import qlearning
 import simple_neural_net_player
+import em.em_player
 
 def get_move(view):
+  return em_player.get_move(view)
   # return simple_neural_net_player.get_move(view)
-  return qlearning.get_move(view)
+  # return qlearning.get_move(view)
   # return data_collector.collect_data(view)
